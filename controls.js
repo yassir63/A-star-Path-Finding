98739327e02f8282bc410f10103848d1d416a3dc
Yassir Amami <amamiyassir@gmail.com>
2022-05-04 13:41:49 +0000

    Final Version -- needs better heuristic

--- controls.js
+++ controls.js
@@ -1,112 +1,53 @@
 let paused = false;
 document.addEventListener('keypress', (e) => {
     if (e.key == "P" || e.key == "p") {
-      if (!paused) {
-                    noLoop()
-                    paused = true;
-                } else {
-                    loop();
-                    paused = false;
-                }
+        if (!paused) {
+            noLoop()
+            paused = true;
+        } else {
+            loop();
+            paused = false;
+        }
     }
 })
 
-function stop(){
-   
-        noLoop()
-        paused = true;
-    
-    }
+function stop() {
 
-function run(){
+    noLoop()
+    paused = true;
 
-        loop();
-           paused = false;
-          
-    }
+}
 
-// var p = document.getElementById("pause");
+function run() {
 
-// p.addEventListener("click", function() {
+    loop();
+    paused = false;
 
-//       if (!paused) {
-//                     noLoop()
-//                     paused = true;
-//                 } else {
-//                     loop();
-//                     paused = false;
-//                 }
-    
-// })
+}
 
-// var s = document.getElementById("start");
 
-// s.addEventListener("click", function() {
-  
-
-//                loop();
-//                paused = false;
-          
-                 
-//   });
 
 
 document.addEventListener('keypress', (s) => {
     if (s.key == " ") {
-      if (!paused) {
-        loop();
-        paused = false;
+        if (!paused) {
+            loop();
+            paused = false;
+        }
     }
-}
 })
 
 
 
 document.addEventListener('keypress', (x) => {
     if (x.key == "S" || x.key == "s") {
-      if (!setUp) {
-                    setUp = true;
-                    loop();
-                } else {
-                    setUp = false;
-                    noLoop();
-                    
-                }
+        if (!setUp) {
+            setUp = true;
+            loop();
+        } else {
+            setUp = false;
+            noLoop();
+
+        }
     }
 })
-
-
-
-
-
-
-// let start = false;
-// document.addEventListener('keypress', (x) => {
-//     if (x.key == "S" || x.key == "s") {
-//       if (start) {
-//                     noLoop()
-//                     start = false;
-//                 } else {
-//                     loop();
-//                     start = true;
-                    
-//                 }
-//     }
-// })
-
-// const btn = document.querySelector('#btn');        
-//         const radioButtons = document.querySelectorAll('input[name="size"]');
-//         btn.addEventListener("click", () => {
-//             let selectedSize;
-//             for (const radioButton of radioButtons) {
-//                 if (radioButton.checked) {
-//                     selectedSize = radioButton.value;
-//                     break;
-//                 }
-//             }
-//             // show the output:
-//             output.innerText = selectedSize ? `You selected ${selectedSize}` : `You haven't selected any size`;
-//         });
-
-// if not start noLoop();
-
--- sketch.js
+++ sketch.js
@@ -10,15 +10,15 @@ var openSet = [];
 var closedSet = [];
 var start;
 var end;
-var w,h;
+var w, h;
 var path = [];
 var mode;
 let setUp = false;
 
-function removeFromArray(arr,elmt){
-  for(var i = arr.length-1;i>=0;i--){
-    if(arr[i] == elmt){
-      arr.splice(i,1);
+function removeFromArray(arr, elmt) {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] == elmt) {
+      arr.splice(i, 1);
     }
   }
 }
@@ -28,10 +28,10 @@ function removeFromArray(arr,elmt){
 //   return d;
 // }
 
-function heuristic(a,b){
-  var d = abs(a.i-b.i)+abs(a.j-b.j);
+function heuristic(a, b) {
+  var d = abs(a.i - b.i) + abs(a.j - b.j);
   // var d = dist(a.i,a.j,b.i,b.j);  
-  return d;             // we can use what is called a manhattan distance or taxi-cab distance
+  return d; // we can use what is called a manhattan distance or taxi-cab distance
 }
 
 // function heuristic(a,b){
@@ -65,7 +65,7 @@ function heuristic(a,b){
 
 
 
-function Spot(i,j){
+function Spot(i, j) {
   this.i = i;
   this.j = j;
   this.f = 0;
@@ -76,35 +76,35 @@ function Spot(i,j){
   this.previous = undefined;
   this.obstacle = false;
   this.clicked = false;
-  
 
-  if(random(1) < 0.3){
+
+  if (random(1) < 0.3) {
     this.obstacle = true;
   }
-  
-  
 
 
 
 
-  this.show = function(col){
+
+
+  this.show = function (col) {
     fill(col);
-    if(this.obstacle){
+    if (this.obstacle) {
       fill(0);
-    } else if(this.clicked){
-      fill(243,67,65);
+    } else if (this.clicked) {
+      fill(243, 67, 65);
     }
     noStroke();
-     rect(this.i*w,this.j*h,w-1,h-1);
-    }
+    rect(this.i * w, this.j * h, w - 1, h - 1);
+  }
 
 
-  this.addNeighbors = function(grid,mode){
+  this.addNeighbors = function (grid, mode) {
     var i = this.i;
     var j = this.j;
-    
 
-    if(mode == 0){
+
+    if (mode == 0) {
 
 
       if (i < cols - 1) {
@@ -135,206 +135,205 @@ function Spot(i,j){
 
 
 
-  
-    }else{
-      if(i<cols-1){
-        this.neighbors.push(grid[i+1][j]);
-      
+
+    } else {
+      if (i < cols - 1) {
+        this.neighbors.push(grid[i + 1][j]);
+
       }
-      if(i>0){
-        this.neighbors.push(grid[i-1][j]);
-   
+      if (i > 0) {
+        this.neighbors.push(grid[i - 1][j]);
+
       }
-  
-      if(j < rows-1){
-        this.neighbors.push(grid[i][j+1]);
- 
+
+      if (j < rows - 1) {
+        this.neighbors.push(grid[i][j + 1]);
+
       }
-  
-      if(j>0){
-        this.neighbors.push(grid[i][j-1]);
-        
+
+      if (j > 0) {
+        this.neighbors.push(grid[i][j - 1]);
+
       }
     }
 
-   
-   
-    
-    
+
+
+
+
   }
-  
+
 }
 
-Spot.prototype.contains = function (x,y){
-  return(x*w>this.x && x*w < (this.x*w + this.w) && y*w > this.y*w && y*w < (this.y*w + this.w));
+Spot.prototype.contains = function (x, y) {
+  return (x * w > this.x && x * w < (this.x * w + this.w) && y * w > this.y * w && y * w < (this.y * w + this.w));
 }
 
 
 function mousePressed() {
-  for(var i=0; i < cols; i++){
-    for( var j=0; j < rows ; j++ ){
-      if(grid[i][j].contains(mouseX,mouseY)){
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      if (grid[i][j].contains(mouseX, mouseY)) {
         grid[i][j].clicked = true;
         console.log("salam");
-        
-}
-}
+
+      }
     }
-     
-    
   }
 
-  
+
+}
+
+
 
 
 function validate() {
   if (document.getElementById('diagonal').checked) {
     openSet = [];
     closedSet = [];
-    
-     setup(0);
-     draw();
-  }else{
+
+    setup(0);
+    draw();
+  } else {
     openSet = [];
     closedSet = [];
-    
-      setup(1);
-      draw();
+
+    setup(1);
+    draw();
   }
 }
 
 function setup(mode) {
-    createCanvas(500, 500);
-    console.log('A star Yassir Amami');
-    
-    w = width/cols;
-    h = height/rows;
-  
-      for(var i = 0 ; i< cols;i++){
-      grid[i] = new Array(rows);
-    }
+  createCanvas(500, 500);
+  console.log('A star Yassir Amami');
 
+  w = width / cols;
+  h = height / rows;
 
-    for(var i = 0 ; i< cols;i++){
-      for(var j=0; j<rows;j++){
-        grid[i][j] = new Spot(i,j);
-    }
-      }
-
-      for(var i = 0 ; i< cols;i++){
-      for(var j=0; j<rows;j++){
-        grid[i][j].addNeighbors(grid,mode);
-    }
-      }
-
-start = grid[46][15];
-if(start.obstacle){
-  window.alert("Le point de départ est un Obstacle ! L'algorithme ne commencera pas !");
-  return 0;
-}
-end = grid[cols-1][rows-1];
-if(end.obstacle){
-  window.alert("Le point de d'arrivée est un obstacle, L'algorithme ne trouvera pas de chemin et testera jusqu'à la fin !");
-}
-
-
-openSet.push(start);
-
-console.log(grid); 
-
-
-
-noLoop();
-      
+  for (var i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
   }
 
 
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].addNeighbors(grid, mode);
+    }
+  }
+
+  start = grid[46][15];
+  if (start.obstacle) {
+    window.alert("Le point de départ est un Obstacle ! L'algorithme ne commencera pas !");
+    return 0;
+  }
+  end = grid[cols - 1][rows - 1];
+  if (end.obstacle) {
+    window.alert("Le point de d'arrivée est un obstacle, L'algorithme ne trouvera pas de chemin et testera jusqu'à la fin !");
+  }
 
 
-  function draw() {
-    
-    if(!setUp){
-     
-    
+  openSet.push(start);
+
+  console.log(grid);
+
+
+
+  noLoop();
+
+}
+
+
+
+
+function draw() {
+
+  if (!setUp) {
+
+
     background(0);
 
-    if(openSet.length > 0){
+    if (openSet.length > 0) {
       var lowestIndex = 0;
-      for (var i=0;i<openSet.length;i++){
-        if(openSet[i].f < openSet[lowestIndex].f){
-          lowestIndex = i; 
+      for (var i = 0; i < openSet.length; i++) {
+        if (openSet[i].f < openSet[lowestIndex].f) {
+          lowestIndex = i;
         }
       }
 
       var current = openSet[lowestIndex];
 
-      if(current === end ){
+      if (current === end) {
 
-      noLoop();
-          document.getElementById("result").innerHTML = "PATH FOUND !";
+        noLoop();
+        document.getElementById("result").innerHTML = "PATH FOUND !";
 
       }
-      
 
-      removeFromArray(openSet,current);
+
+      removeFromArray(openSet, current);
       closedSet.push(current);
 
       var neighbors = current.neighbors;
-      for(var i = 0;i<neighbors.length;i++){
+      for (var i = 0; i < neighbors.length; i++) {
         var neighbor = neighbors[i];
-        if(!closedSet.includes(neighbor) && !neighbor.obstacle){
+        if (!closedSet.includes(neighbor) && !neighbor.obstacle) {
           var tempG = current.g + 1;
-var newPath = false;
-          if(openSet.includes(neighbor)){
-            if(tempG < neighbor.g){
-              neighbor.g = tempG;      // tempG to check if g of node was smaller in a previous iteration to keep it
+          var newPath = false;
+          if (openSet.includes(neighbor)) {
+            if (tempG < neighbor.g) {
+              neighbor.g = tempG; // tempG to check if g of node was smaller in a previous iteration to keep it
               newPath = true;
             }
-          }else{
+          } else {
             neighbor.g = tempG;
             newPath = true;
             openSet.push(neighbor);
           }
-          if(newPath){
-            neighbor.h = heuristic(neighbor,end);
-          neighbor.f = neighbor.g + neighbor.h;
-          neighbor.previous = current;
+          if (newPath) {
+            neighbor.h = heuristic(neighbor, end);
+            neighbor.f = neighbor.g + neighbor.h;
+            neighbor.previous = current;
           }
-          
+
 
         }
       }
-    }else{
-      
+    } else {
+
       document.getElementById("result").innerHTML = "PATH NOT FOUND !";
       // no solution !
     }
 
-    for(var i =0 ; i<cols;i++){
-      for(var j =0; j<rows ; j++){
+    for (var i = 0; i < cols; i++) {
+      for (var j = 0; j < rows; j++) {
         grid[i][j].show(color(255));
       }
     }
 
-    for(var i =0 ; i<closedSet.length;i++){
-      closedSet[i].show(color(255,0,0));
+    for (var i = 0; i < closedSet.length; i++) {
+      closedSet[i].show(color(255, 0, 0));
     }
 
-    for(var i =0 ; i<openSet.length;i++){
-      openSet[i].show(color(0,255,0));      
+    for (var i = 0; i < openSet.length; i++) {
+      openSet[i].show(color(0, 255, 0));
     }
 
     path = [];
     var temp = current;
     path.push(temp);
-    while(temp.previous){
-    path.push(temp.previous);   // back tracks the path starting from the end or every frame
-    temp = temp.previous;
+    while (temp.previous) {
+      path.push(temp.previous); // back tracks the path starting from the end or every frame
+      temp = temp.previous;
     }
 
-    for(var i=0;i<path.length;i++){
-      path[i].show(color(0,0,255));
+    for (var i = 0; i < path.length; i++) {
+      path[i].show(color(0, 0, 255));
     }
   }
 }
-  
