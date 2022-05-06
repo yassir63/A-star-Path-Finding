db02f6d330b6caa45115fa930d95f6f0cf865796
Yassir Amami <amamiyassir@gmail.com>
2022-05-06 00:25:35 +0000

    Troubleshooting

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '98739327e02f8282bc410f10103848d1d416a3dc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '98739327e02f8282bc410f10103848d1d416a3dc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
